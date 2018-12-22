151e6426bcbe50c923020f921de9dca0d0359094
Jon Alberdi <j.alberdi@criteo.com>
2018-12-22 11:27:26 +0100

    Refactor EntriesInMultipleJars

--- src/main/groovy/com/criteo/gradle/findjars/conflicts/EntriesInMultipleJars.groovy
+++ src/main/groovy/com/criteo/gradle/findjars/conflicts/EntriesInMultipleJars.groovy
@@ -21,39 +21,29 @@ class EntriesInMultipleJars {
 
     Map<ConflictingJars, Set<String>> factorize() {
         Map<ConflictingJars, Set<String>> res = new HashMap<>()
-        entriesInMultipleJars.each {
-             key , value ->
+        entriesInMultipleJars.each { key , value ->
             ConflictingJars newKey = new ConflictingJars(value)
             Set<String> related = res.getOrDefault(newKey, new HashSet<>())
             related.add(key)
             res[newKey] = related
         }
-        (Map<ConflictingJars, Set<String>>)res.collectEntries {key, value -> [key: value.toSet()] }
+        (Map<ConflictingJars, Set<String>>)res.collectEntries {key, value -> [(key): value.toSet()] }
     }
 
     private static Map<String, Set<String>> build(Collection<JarFileAndEntry> entries) {
-        Map<String, Set<JarFileAndEntry>> entriesToJars = findEntriesWithSamePathInDifferentJars(entries)
-        Map<String, Map<String, Set<JarFileAndPath>>> entriesPerDigest = new HashMap<>()
-        for (Map.Entry<String, Set<JarFileAndEntry>> entryToJars: entriesToJars) {
-            Map<String, Set<JarFileAndPath>> jarsGroupedByEntriesDigest = new HashMap<>()
-            for (JarFileAndEntry entry : entryToJars.getValue()) {
-                String digest = getDigest(entry.getJarFile(), entry.getJarEntry())
-                Set<JarFileAndPath> related = jarsGroupedByEntriesDigest.getOrDefault(digest, new HashSet<>())
-                related.add(entry.getJarFile())
-                jarsGroupedByEntriesDigest[digest] = related
-            }
-            entriesPerDigest[entryToJars.getKey()] = jarsGroupedByEntriesDigest
-        }
-        (Map<String, Set<String>>) entriesPerDigest.findAll { className, perDigest ->
-            perDigest.keySet().size() > 1
-        }.collectEntries { key, value ->
-            Collection<Set<JarFileAndPath>> values = value.values()
-            Set<String> paths = values.collectMany { val -> val.collect { it.getJarPath() } }.toSet()
-            [(key): paths]
-        }
+        filterPathsHavingDifferentDigest(groupPathsByDigest(groupEntriesByPath(entries)))
     }
 
-    private static Map<String, Set<JarFileAndEntry>> findEntriesWithSamePathInDifferentJars(Collection<JarFileAndEntry> entries) {
+    private static Map<String, Map<String, Set<JarFileAndPath>>> groupPathsByDigest(Map<String,
+            Set<JarFileAndEntry>> entriesByPath) {
+        Map<String, Map<String, Set<JarFileAndPath>>> entriesByDigest = new HashMap<>()
+        for (Map.Entry<String, Set<JarFileAndEntry>> pathAndEntries: entriesByPath) {
+            entriesByDigest[pathAndEntries.getKey()] = groupEntriesByDigest(pathAndEntries.getValue())
+        }
+        entriesByDigest
+    }
+
+    private static Map<String, Set<JarFileAndEntry>> groupEntriesByPath(Collection<JarFileAndEntry> entries) {
         Map<String, Set<JarFileAndEntry>> elementsToJars = new HashMap<>()
         for (JarFileAndEntry entry : entries) {
             String key = entry.getJarEntry().getName()
@@ -64,6 +54,28 @@ class EntriesInMultipleJars {
         elementsToJars.findAll {
             key, value -> value.size() > 1
         }
+    }
+
+    private static Map<String, Set<String>> filterPathsHavingDifferentDigest(Map<String, Map<String,
+            Set<JarFileAndPath>>> entriesByDigest) {
+        (Map<String, Set<String>>) entriesByDigest.findAll { className, perDigest ->
+            perDigest.keySet().size() > 1
+        }.collectEntries { key, value ->
+            Collection<Set<JarFileAndPath>> values = value.values()
+            Set<String> paths = values.collectMany { val -> val.collect { it.getJarPath() } }.toSet()
+            [(key): paths]
+        }
+    }
+
+    private static Map<String, Set<JarFileAndPath>> groupEntriesByDigest(Collection<JarFileAndEntry> entries) {
+        Map<String, Set<JarFileAndPath>> res = new HashMap<>()
+        for (JarFileAndEntry entry : entries) {
+            String digest = getDigest(entry.getJarFile(), entry.getJarEntry())
+            Set<JarFileAndPath> related = res.getOrDefault(digest, new HashSet<>())
+            related.add(entry.getJarFile())
+            res[digest] = related
+        }
+        res
     }
 
     private static String getDigest(JarFileAndPath jar, JarEntry entry) {
