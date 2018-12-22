40395438673142b906dc0f76c70d4d2c971e1b67
Jon Alberdi <j.alberdi@criteo.com>
2018-12-22 10:27:53 +0100

    Add ConflictingJars

---
+++ src/main/groovy/com/criteo/gradle/findjars/ConflictingJars.groovy
@@ -0,0 +1,28 @@
+package com.criteo.gradle.findjars
+
+class ConflictingJars {
+    Set<String> conflictingJars
+    int hash
+
+    ConflictingJars(Set<String> conflictingJars) {
+        this.conflictingJars = conflictingJars
+        this.hash = String.join(";", conflictingJars.sort()).hashCode()
+    }
+
+    Set<String> getJars() {
+        conflictingJars
+    }
+
+    @Override
+    boolean equals(o) {
+        if (this.is(o)) return true
+        if (getClass() != o.class) return false
+        ConflictingJars that = (ConflictingJars) o
+        return conflictingJars.equals(that.conflictingJars)
+    }
+
+    @Override
+    int hashCode() {
+        this.hash
+    }
+}
--- src/main/groovy/com/criteo/gradle/findjars/FindJarsTask.groovy
+++ src/main/groovy/com/criteo/gradle/findjars/FindJarsTask.groovy
@@ -191,30 +191,4 @@ class FindJarsTask extends DefaultTask {
         return name.matches(jarFilter)
     }
 
-    private class ConflictingJars {
-        Set<String> conflictingJars
-        int hash
-
-        ConflictingJars(Set<String> conflictingJars) {
-            this.conflictingJars = conflictingJars
-            this.hash = String.join(";", conflictingJars.sort()).hashCode()
-        }
-
-        Set<String> getJars() {
-            conflictingJars
-        }
-
-        @Override
-        boolean equals(o) {
-            if (this.is(o)) return true
-            if (getClass() != o.class) return false
-            ConflictingJars that = (ConflictingJars) o
-            return conflictingJars.equals(that.conflictingJars)
-        }
-
-        @Override
-        int hashCode() {
-            this.hash
-        }
-    }
 }
